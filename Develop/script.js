1de2974adafd4ee13a9f221932d7f5d924c6b611
bluesatyr <shawnevans.music@gmail.com>
2020-12-17 19:46:17 -0500

    initial javascript code

--- Develop/script.js
+++ Develop/script.js
@@ -9,17 +9,64 @@ function generatePassword() {
         continue;
     } else {
         window.alert("Your password length is outside of acceptable parameters! Try again!");
-    }
+    };
+    console.log(pLength);
     
     // prompt for special character types
-    var sChar = [];
+    var charTypes = []; // an array to store the boolean values of selected special character types. If true they are included, if false they are not included.
     
-    window.prompt("Do you wish to include lowercase characters? Type 'y' for yes or 'n' for no.")
+    // Prompt for types. 
     
-    // catch invalid input
-    if ()
+    //Can i make a for loop to iterate over each type??
     
-    console.log(pLength)
+    // function characterTypes() {...}
+    var lCase = window.prompt("Do you wish to include lowercase characters? Type 'y' for yes or 'n' for no.")
+    
+    // catch invalid input, similar for each
+    if (lCase === "y"){
+        charTypes[0] = true;
+    } else if (lCase === "y") {
+        charTypes[0] = false;       
+    } else {
+        window.alert("You have made an invalid selection. Please try again.");
+    }
+    
+    // return charTypes
+    
+        
+
+        function (pLength, charTypes) {
+            var result  = '';
+            var charset = '';
+            const charOptions = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '0123456789', '"!#$%&()*+,-/:;<=>?@[\]^_`{|}~_'];
+            
+            // loop to update charset from user criteria
+            for (var i=0; i<4; i++){
+                if (charTypes[i] === true){
+                    charset = charset + charOptions[i];
+                }
+            }
+            console.log(charset);
+            
+            // random string created from charset and length 
+            var charsetLength = charset.length;
+            for ( var j = 0; j < pLength; j++ ) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+            }
+
+        console.log(makeid(5));
+        
+        
+        
+        
+        */
+                       
+        for (var i=0; i<l; i++){
+            nChar = 
+        }
+    }   
 }
 
 // Get references to the #generate element
