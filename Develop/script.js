daee43796cc0996de3ea9cc4887a09689bb3a823
bluesatyr <shawnevans.music@gmail.com>
2020-12-17 22:02:43 -0500

    first working version

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,8 @@
 function generatePassword() {
     // prompt for password length criteria
     var pLength = prompt("Choose a password length from 8 characters up to 128 characters");
-    // check to make sure it is a number from 8 and 128 characters long
-    if (parseInt(pLength) <= 8 || parseInt(pLength) >= 128){
+    // check to make sure it is a number from 8 to 128 characters long
+    if (parseInt(pLength) < 8 || parseInt(pLength) > 128){
         alert("Your password length is outside of acceptable parameters! Try again!");
     } else {
         
@@ -17,37 +17,12 @@ function generatePassword() {
     
     // Prompt for types.
     
-/*
-    if (window.confirm("Do you wish to include lowercase letters? \n Select 'OK' for yes, 'Cancel' for no.")) {
-        charTypes[0] = true;
-    } else {
-        charTypes[0] = false;
-    }
-    
-    if (window.confirm("Do you wish to include uppercase letters? \n Select 'OK' for yes, 'Cancel' for no.")) {
-        charTypes[1] = true;
-    } else {
-        charTypes[1] = false;
-    }
-    
-    if (window.confirm("Do you wish to include numbers? \n Select 'OK' for yes, 'Cancel' for no.")) {
-        charTypes[2] = true;
-    } else {
-        charTypes[2] = false;
-    }
-    
-    if (window.confirm("Do you wish to include special characters? \n Select 'OK' for yes, 'Cancel' for no.")) {
-        charTypes[3] = true;
-    } else {
-        charTypes[3] = false;
-    }
-    
-*/ 
+
     
     const promptTypes = ["lowercase letters", "uppercase letters", "numbers", "special characters"];
     
     for (var i=0; i<4; i++){
-        tPrompt = prompt("Do you wish to include " + promptTypes[i] +"? \n Select 'OK' for yes, 'Cancel' for no.");
+        tPrompt = confirm("Do you wish to include " + promptTypes[i] +"? \n Select 'OK' for yes, 'Cancel' for no.");
         charTypes[i] = tPrompt;
     }
     
@@ -64,32 +39,28 @@ function generatePassword() {
     
         
 
-        function randomize(pLength, charTypes) { //is it necessary to roll this within another function?
-            var result  = '';
-            var charset = '';
-            const charOptions = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '0123456789', '"!#$%&()*+,-/:;<=>?@[\]^_`{|}~_'];
-            
-            // loop to update charset from user criteria
-            for (var i=0; i<4; i++){
-                if (charTypes[i] === true){
-                    charset = charset + charOptions[i];
-                }
-            }
-            console.log(charset);
-            
-            // random string created from charset and length 
-            var charsetLength = charset.length;
-            for ( var j = 0; j < pLength; j++ ) {
-                result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            }
-            return result;
-            }
 
-            //console.log(makeid(5));
+    var result  = '';
+    var charset = '';
+    const charOptions = ['abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '0123456789', '"!#$%&()*+,-/:;<=>?@[\]^_`{|}~_'];
+    
+    // loop to update charset from user criteria
+    for (var i=0; i<4; i++){
+        if (charTypes[i] === true){
+            charset = charset + charOptions[i];
+        }
+    }
+    console.log(charset);
+    
+    // random string created from charset and length 
+    var charsetLength = charset.length;
+    for ( var j = 0; j < pLength; j++ ) {
+        result += charset.charAt(Math.floor(Math.random() * charsetLength));
+    }
+    console.log(result);
+    return result;
+    }
         
-    }   
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
